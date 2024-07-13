1cbdbf167ced5147d0bdad2522ab7bc5bfb78658
Tami Vahab <tm80009000@gmail.com>
2024-07-13 22:28:52 +0300

    update

--- src/components/Layout Components/Home.js
+++ src/components/Layout Components/Home.js
@@ -8,6 +8,7 @@ export const Home = () => {
     return (
        <div>
         <h1>hello from tami</h1>
+        <h2>tami would like to show you that its work!! BH!!!!!!!11</h2>
         <Banner/>
         <RecommendedProducts/>
         <StayTuned></StayTuned>
